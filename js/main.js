405d4984e4e42c8f785a3a09f5e0c43853ade6b8
Nishimura <sigotoyou.mail00@gmail.com>
2024-02-23 11:27:10 +0900

    canvas要素を生成

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 
 
+let canvas = document.createElement('canvas');// 生成
+canvas.width = 1200; // 横幅設定
+canvas.height=800; // 高さ設定
+let context = canvas.getContext('2d'); // 2Dコンテキスト
 
-document.addEventListener('DOMContentLoaded',function(){
-    document.querySelector('#target').textContent='文字列を出力';
-})
+console.log(canvas);
+console.log(context);
