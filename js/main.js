35b7248c75646c2a8c674909acbe49070d9f10ff
Nishimura <sigotoyou.mail00@gmail.com>
2024-02-23 12:49:57 +0900

    canvasをindex.htmlに表示

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
-
-
+document.addEventListener('DOMContentLoaded',function(){
 let canvas = document.createElement('canvas');// 生成
 canvas.width = 1200; // 横幅設定
 canvas.height=800; // 高さ設定
 let context = canvas.getContext('2d'); // 2Dコンテキスト
 
-console.log(canvas);
-console.log(context);
+canvas.style.background = 'url(image/bg.png)';// 背景画像を設定
+let target = document.querySelector('#target'); // idがtartgetの要素を選択
+target.append(canvas); // canvas要素を追加
+
+});
